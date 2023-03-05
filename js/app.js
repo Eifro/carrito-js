c9609851b3f623a554f36fa1bd62ff9785d569bf
eifro <afrotricks19@gmail.com>
2023-03-05 16:58:50 -0500

    add btn delete

--- js/app.js
+++ js/app.js
@@ -40,17 +40,21 @@ function leerDatosCurso(curso)
 
 function mostrarCursoCarrito(cursos)
 {
-    limpiarCarrito()
+    limpiarCarrito() // limpiar carrito html
     cursos.forEach(curso => { // agregar array productosCarrito a carritoBody
+        const {id, imagen, titulo, precio, cantidad} = curso // destructuring object
         carritoBody.innerHTML += // template literal para agregar html del carrito
         `
-            <tr data-id=${curso.id}>
-                <th>
-                    <img src="${curso.imagen}" width="150">
-                </th>
-                <th>${curso.titulo}</th>
-                <th>${curso.precio}</th>
-                <th>${curso.cantidad}</th>
+            <tr>
+                <td>
+                    <img src="${imagen}" width="100">
+                </td>
+                <td>${titulo}</td>
+                <td>${precio}</td>
+                <td>${cantidad}</td>
+                <td>
+                    <a href="#" class="borrar-curso" data-id=="${id}">X</a>
+                </td>
             </tr>
         ` 
     })   
